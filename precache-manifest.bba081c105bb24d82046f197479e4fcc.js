14ac631f757133769de154502b13af8e506e5c03
Glenn <zaku0225@gmail.com>
2022-03-04 16:51:21 +0800

    First commit.

---
+++ precache-manifest.bba081c105bb24d82046f197479e4fcc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de9a1a2fdd00b19f4dd175e5385230da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97a7018a00a9ab921186",
+    "url": "/static/css/main.94da59e8.chunk.css"
+  },
+  {
+    "revision": "e133ae9f032a8042acbb",
+    "url": "/static/js/2.7bcbc013.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.7bcbc013.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97a7018a00a9ab921186",
+    "url": "/static/js/main.b470e7e1.chunk.js"
+  },
+  {
+    "revision": "c26ef94ce5349e6b5cae",
+    "url": "/static/js/runtime-main.31bd3342.js"
+  }
+]);
